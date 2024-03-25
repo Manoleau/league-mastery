c10d8a0715de9a5aee853faaff826cae97460193
Emmanuel Ardoin <emmanuelardoin@gmail.com>
2024-03-26 00:42:35 +0100

    Dépendence pour faire graphique

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,14 @@ android {
         viewBinding = true
     }
 }
-
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    implementation("com.github.Philjay:MPAndroidChart:v3.1.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
